96ecb36cbcb5452b618d2074fe5c90168a0da16a
Benjamen King <brk.3rdof3kings@gmail.com>
2018-04-07 15:05:26 -0600

    Set up localhost DB using DB_SETUP.sql

--- DB_SETUP.sql
+++ DB_SETUP.sql
@@ -6,3 +6,55 @@ CREATE TABLE users (
   PRIMARY KEY (id),
   UNIQUE KEY email (email)
 );
+
+CREATE TABLE vaults (
+  id int NOT NULL AUTO_INCREMENT,
+  name VARCHAR(20) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  userId VARCHAR(255),
+  INDEX userId (userId),
+  FOREIGN KEY (userId)
+    REFERENCES users(id)
+    ON DELETE CASCADE,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE keeps (
+  id int NOT NULL AUTO_INCREMENT,
+  name VARCHAR(20) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  userId VARCHAR(255),
+  INDEX userId (userId),
+  FOREIGN KEY (userId)
+    REFERENCES users(id)
+    ON DELETE CASCADE,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE vaultkeeps (
+  id int NOT NULL AUTO_INCREMENT,
+  vaultId int NOT NULL,
+  keepId int NOT NULL,
+  userId VARCHAR(255) NOT NULL,
+
+  PRIMARY KEY (id),
+  INDEX (vaultId, keepId),
+  INDEX (userId),
+  
+  FOREIGN KEY (userId)
+    REFERENCES users(id)
+    ON DELETE CASCADE,
+
+  FOREIGN KEY (vaultId)
+    REFERENCES vaults(id)
+    ON DELETE CASCADE,
+
+  FOREIGN KEY (keepId)
+    REFERENCES keeps(id)
+    ON DELETE CASCADE
+);
+
+-- -- USE THIS LINE FOR GET KEEPS BY VAULT ID
+-- SELECT * FROM vaultkeeps
+-- INNER JOIN keeps ON keeps.id = vaultkeeps.keepId
+-- WHERE (vaultId = 2)   -- ("2" is just an example)
