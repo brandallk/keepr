02c4813cc1729baa9acf26cdae64801bfb89341c
Benjamen King <brk.3rdof3kings@gmail.com>
2018-04-10 20:44:34 -0600

    Get userId from HttpContext when GETting keeps or vaults by user ID

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,12 +1,13 @@
 using System.Collections.Generic;
 using keepr.Models;
 using keepr.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace keepr.Controllers
 {
     [Route("api/[controller]")]
-    public class UsersController
+    public class UsersController : Controller
     {
         private readonly UserRepository _repo;
         private readonly KeepsRepository _keepsRepo;
@@ -23,14 +24,34 @@ namespace keepr.Controllers
         // Get keeps by User ID
         public List<Keep> GetKeepsByUserId(string id)
         {
-            return _keepsRepo.GetByUserId(id);
+            // Get the userId from the users's erver session as more-secure alternative to passing it in from the front-end:
+            var user = HttpContext.User;
+            if (user.Identity.Name != null)
+            {
+                var sessionId = user.Identity.Name;
+                return _keepsRepo.GetByUserId(sessionId);
+            }
+            return null;
+
+            // Alternative -- Use the userId passed in from the front end as a route parameter:
+            // return _keepsRepo.GetByUserId(id);
         }
 
         [HttpGet("{id}/vaults")]
         // Get vaults by User ID
         public List<Vault> GetVaultsByUserId(string id)
         {
-            return _vaultsRepo.GetByUserId(id);
+            // Get the userId from the users's erver session as more-secure alternative to passing it in from the front-end:
+            var user = HttpContext.User;
+            if (user.Identity.Name != null)
+            {
+                var sessionId = user.Identity.Name;
+                return _vaultsRepo.GetByUserId(sessionId);
+            }
+            return null;
+
+            // Alternative -- Use the userId passed in from the front end as a route parameter:
+            // return _vaultsRepo.GetByUserId(id);
         }
     }
 }
