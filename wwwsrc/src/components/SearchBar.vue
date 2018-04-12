932899bd8c7a2b4842262e6b9518fac6213e65ec
Benjamen King <brk.3rdof3kings@gmail.com>
2018-04-12 14:32:11 -0600

    Finish creating the search feature on Welcome page

--- wwwsrc/src/components/SearchBar.vue
+++ wwwsrc/src/components/SearchBar.vue
@@ -1,13 +1,21 @@
 <template>
-  <div class="search-bar p-3 d-flex justify-content-center">
+  <div class="search-bar">
+    <div class="p-3 d-flex justify-content-center">
+  
+      <form class="form-inline" @submit.prevent="submit">
+        <div class="form-group">
+          <label for="search">Search: </label>
+          <input type="text" class="ml-2" name="search" v-model="searchQuery">
+        </div>
+        <button type="submit" class="btn btn-default btn-sm ml-2">search</button>
+        <button type="button" class="btn btn-default btn-sm ml-2" v-if="showingSearchResults" @click="hideSearchResults">back</button>
+      </form>
+        
+    </div>
 
-    <form class="form-inline" @submit.prevent="submit">
-      <div class="form-group">
-        <label for="search">Search: </label>
-        <input type="text" class="ml-2" name="search" v-model="searchQuery">
-      </div>
-      <button type="submit" class="btn btn-default btn-sm ml-2">search</button>
-    </form>
+    <div class="text-center" v-if="showingSearchResults">
+      <p>Search Results:</p>
+    </div>
 
   </div>
 </template>
@@ -21,7 +29,8 @@
     props: [],
     data() {
       return {
-        searchQuery: ""
+        searchQuery: "",
+        showingSearchResults: false
       }
     },
     computed: {
@@ -30,6 +39,12 @@
     methods: {
       submit() {
         this.$store.dispatch('searchKeeps', this.searchQuery)
+        this.searchQuery = ""
+        this.showingSearchResults = true
+      },
+      hideSearchResults() {
+        this.$store.dispatch('getPublicKeeps')
+        this.showingSearchResults = false
       }
     }
   }
